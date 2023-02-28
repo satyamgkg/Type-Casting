c0c88d14a2a1e3b3b78f1500911d9e757ea2d8d4
satyamgkg <97548391+satyamgkg@users.noreply.github.com>
2023-02-28 17:36:29 +0530

    Add files via upload

---
+++ TypeCastingExample.java
@@ -0,0 +1,20 @@
+// Type Casting 
+
+
+public class TypeCastingExample  
+{  
+  public static void main(String[] args)  
+{  
+   int x = 7;  
+
+   long y = x;  
+
+   float z = y;  
+
+   System.out.println("Before conversion, int value "+x);  
+
+   System.out.println("After conversion, long value "+y);  
+
+   System.out.println("After conversion, float value "+z);  
+ }  
+}  
---
+++ TypeCastingExample2.java
@@ -0,0 +1,20 @@
+// Type Casting
+
+
+public class TypeCastingExample2 
+{  
+  public static void main(String args[])  
+{  
+   double d = 166.66;  
+
+   long l = (long)d;  
+  
+   int i = (int)l;
+  
+   System.out.println("Before conversion: "+d);  
+
+   System.out.println("After conversion into long type: "+l);  
+
+   System.out.println("After conversion into int type: "+i);  
+ }  
+}  
